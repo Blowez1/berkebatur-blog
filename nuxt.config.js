68371b9587734871ba11de45b6abdb4017591bd6
Berke Batur <aberkebatur@gmail.com>
2021-09-18 12:16:21 +0300

    added components

---
+++ nuxt.config.js
@@ -0,0 +1,54 @@
+
+const desc = ""
+
+export default {
+  
+  head: {
+    titleTemplate: '%s - Berke Batur Blog',
+    title: 'Berke Batur Blog',
+    htmlAttrs: {
+      lang: 'tr'
+    },
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: desc
+      },
+      {
+        name: 'format-detection',
+        content: 'telephone=no'
+      }
+    ],
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.png'
+    }]
+  },
+
+  css: [
+    '@/assets/scss/main.scss'
+  ],
+
+  plugins: [],
+
+  components: true,
+
+  buildModules: [],
+
+  loading: {
+    color: '#CD2027',
+    height: '4px'
+  },
+
+  modules: [],
+
+  build: {}
+}
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Şuan Anasayfadasınız...</div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      titleTemplate: '%s',
+      title: 'Berke Batur Blog'
+    }
+  }
+}
+</script>
